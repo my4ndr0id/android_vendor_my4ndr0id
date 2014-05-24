de1778508f2df377078329767c5ba3fd09293aaf
my4ndr0id <Team.my.android@gmail.com>
2014-05-25 02:34:27 +0700

    add ota-update-centre app, add missing efsbackup

--- config/common.mk
+++ config/common.mk
@@ -31,7 +31,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     backuptool.sh \
     backuptool.functions \
-    50-my4ndr0id.sh
+    50-my4ndr0id.sh \
+    efsbackup.sh
 
 # init.d support
 PRODUCT_PACKAGES += \
@@ -134,3 +135,15 @@ endif
 PRODUCT_PROPERTY_OVERRIDES += \
   ro.my4ndr0id.version=$(my4ndr0id_VERSION) \
   ro.modversion=$(my4ndr0id_VERSION)
+
+otatime := $(shell date +%Y%m%d-%l%M)
+
+# OTA Update Center settings
+PRODUCT_PROPERTY_OVERRIDES += \
+    otaupdater.otaid=my4ndr0id-eg909 \
+    otaupdater.otaver=$(my4ndr0id_VERSION) \
+    otaupdater.otatime=$(otatime) \
+    otaupdater.sdcard.recovery=sdcard
+
+PRODUCT_PACKAGES += \
+    ota-update-centre
