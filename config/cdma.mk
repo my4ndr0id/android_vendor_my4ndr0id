c852bacf03365929b7b9c73bfafa4145d8458642
alexforsale <alexforsale@yahoo.com>
2014-06-05 23:48:29 +0700

    cdma.mk & gsm.mk
    	cm9 apns-conf.xml and spn-conf.xml

---
+++ config/cdma.mk
@@ -0,0 +1,3 @@
+# CDMA APN list
+PRODUCT_COPY_FILES += \
+    vendor/my4ndr0id/prebuilt/etc/apns-conf-cdma.xml:system/etc/apns-conf.xml
---
+++ config/gsm.mk
@@ -0,0 +1,12 @@
+# GSM APN list
+PRODUCT_COPY_FILES += \
+    vendor/my4ndr0id/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# GSM SPN overrides list
+PRODUCT_COPY_FILES += \
+    vendor/my4ndr0id/prebuilt/etc/spn-conf.xml:system/etc/spn-conf.xml
+
+# SIM Toolkit
+PRODUCT_PACKAGES += \
+    Stk
+
